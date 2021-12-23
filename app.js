3496a86ec584326c1edd3af93e8723ad45468018
scottsak <48599109+scottsak@users.noreply.github.com>
2021-12-23 13:23:40 -0800

    added function to button

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const colorSequence = [];
 
+const colorSelection=["red","green","blue", "yellow"];
+
+let d = document.querySelect("red").addEventListener("click", test);
+
+d.onclick()=test();
+
+function test(){
+  alert("works");
+}
 
 function nextSequence(){
   return Math.floor(Math.random() * 4);
