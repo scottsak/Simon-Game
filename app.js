343a372c07008a4fbf25841ad0d47560a091146d
scottsak <48599109+scottsak@users.noreply.github.com>
2021-12-25 10:44:41 -0800

    added the flashing of the lights function for the sequence

--- app.js
+++ app.js
@@ -1,23 +1,59 @@
 const colorSequence = [];
-userInput=[];
-const colorSelection=["red","green","blue", "yellow"];
+const userInput = [];
+const colorSelection = ["red", "green", "blue", "yellow"];
+let correct = true;
 
 
 
-
-function addRed(){
+function addRed() {
   userInput.push("red");
-}
-function addBlue(){
-  userInput.push("blue");
-}
-function addYellow(){
-  userInput.push("yellow");
-}
-function addGreen(){
-  userInput.push("green");
-}
+};
 
-function nextSequence(){
+function addBlue() {
+  userInput.push("blue");
+};
+
+function addYellow() {
+  userInput.push("yellow");
+};
+
+function addGreen() {
+  userInput.push("green");
+};
+
+function nextSequence() {
   return Math.floor(Math.random() * 4);
+};
+
+function startGame() {
+  while (correct === true) {
+    switch (nextSequence()) {
+      case 0:
+        colorSequence.push("red");
+        document.getElementById("red").style.backgroundColor = "purple";
+        setTimeout(() => {  document.getElementById("red").style.backgroundColor = "red"; }, 20000);
+        break;
+      case 1:
+        colorSequence.push("green");
+        document.getElementById("green").style.backgroundColor = "purple";
+        setTimeout(() => {  document.getElementById("green").style.backgroundColor = "green"; }, 2000);
+        break;
+      case 2:
+        colorSequence.push("blue");
+        document.getElementById("blue").style.backgroundColor = "purple";
+        setTimeout(() => {  document.getElementById("blue").style.backgroundColor = "blue"; }, 2000);
+        break;
+      case 3:
+        colorSequence.push("yellow")
+        document.getElementById("yellow").style.backgroundColor = "purple";
+        setTimeout(() => {  document.getElementById("yellow").style.backgroundColor = "yellow"; }, 2000);
+        break;
+    }
+    if (userInput === colorSequence) {
+      alert("you got it right");
+    } else {
+      alert("You got it wrong");
+      break;
+    }
+  }
 }
