44eabc723e018c71e56bcab912d2762b92210712
scottsak <48599109+scottsak@users.noreply.github.com>
2021-12-26 23:44:58 -0800

    added more functionality to the game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const colorSequence = [];
-const userInput = [];
+let colorSequence = [];
+let userInput = [];
 const colorSelection = ["red", "green", "blue", "yellow"];
 let correct = true;
 
@@ -25,34 +25,49 @@ function nextSequence() {
   return Math.floor(Math.random() * 4);
 };
 
+function getNextNum(nextColor){
+  switch (nextColor) {
+    case 0:
+      colorSequence.push("red");
+      // document.getElementById("red").style.backgroundColor = "purple";
+      // setTimeout(() => {  document.getElementById("red").style.backgroundColor = "red"; }, 20000);
+      break;
+    case 1:
+      colorSequence.push("green");
+      // document.getElementById("green").style.backgroundColor = "purple";
+      // setTimeout(() => {  document.getElementById("green").style.backgroundColor = "green"; }, 2000);
+      break;
+    case 2:
+      colorSequence.push("blue");
+
+      break;
+    case 3:
+      colorSequence.push("yellow")
+      // document.getElementById("yellow").style.backgroundColor = "purple";
+      // setTimeout(() => {  document.getElementById("yellow").style.backgroundColor = "yellow"; }, 2000);
+      break;
+  }
+}
+
 function startGame() {
+  console.log(colorSequence);
+  colorSequence = [];
+  console.log(colorSequence);
+  userInput = [];
+  correct=true;
   while (correct === true) {
-    switch (nextSequence()) {
-      case 0:
-        colorSequence.push("red");
-        document.getElementById("red").style.backgroundColor = "purple";
-        setTimeout(() => {  document.getElementById("red").style.backgroundColor = "red"; }, 20000);
-        break;
-      case 1:
-        colorSequence.push("green");
-        document.getElementById("green").style.backgroundColor = "purple";
-        setTimeout(() => {  document.getElementById("green").style.backgroundColor = "green"; }, 2000);
-        break;
-      case 2:
-        colorSequence.push("blue");
-        document.getElementById("blue").style.backgroundColor = "purple";
-        setTimeout(() => {  document.getElementById("blue").style.backgroundColor = "blue"; }, 2000);
-        break;
-      case 3:
-        colorSequence.push("yellow")
-        document.getElementById("yellow").style.backgroundColor = "purple";
-        setTimeout(() => {  document.getElementById("yellow").style.backgroundColor = "yellow"; }, 2000);
-        break;
-    }
+    getNextNum(nextSequence());
+    colorSequence.forEach(function (item, index) {
+      document.getElementById(item).style.backgroundColor = "purple";
+      setTimeout(() => {  document.getElementById(item).style.backgroundColor = item; }, 2000);
+    });
+    setTimeout(() => {  console.log("pause"); }, 4000);
+    console.log("pause over");
     if (userInput === colorSequence) {
-      alert("you got it right");
+      // alert("you got it right");
     } else {
-      alert("You got it wrong");
+      // alert("You got it wrong");
+      correct=false;
       break;
     }
   }
