3695c648ffbadbba1c14eb6ab3e2086a36822110
scottsak <48599109+scottsak@users.noreply.github.com>
2021-12-29 04:22:32 -0800

    added some new methods and got btn semi funtioning

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ $(document).keypress(function() {
   if (!started) {
     $("#level").text("Level " + level);
     //start game
-    startGame();
+    showSequence();
     //get colorSequence
+    //sleep((level*1000)+1000);
     //see if it matches
     started=true;
   }
@@ -20,12 +21,12 @@ $(document).keypress(function() {
 
 //-------------------------------------
 
-function startGame() {
+function showSequence() {
   level++;
   getNextColor();
   for(let i=0; i<colorSequence.length; i++){
     lightUp(colorSelection.indexOf(colorSequence[i]));
-    sleep(2000);
+    // sleep(1000);
   }
 };
 
@@ -33,12 +34,34 @@ function startGame() {
 
 //-------------------------------------
 //gets user input
-$("btn").click(function(){
-  userInput.push($(this).attr("id"));
+$(".btn").click(function(){
+  let x = $(this).attr("id");
+  userInput.push(x);
+
+  //count++
+  //if count = length of level then check checkAnswer
+
+  checkAnswer();
 });
 
 //-------------------------------------
-
+//check if wrong or right
+function checkAnswer(){
+  console.log(userInput);
+  console.log(colorSequence);
+  if(userInput===colorSequence){
+    correct=true;
+  }
+  else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level").text("Game Over, Press Any Key to Restart");
+    level = 0;
+    gamePattern = [];
+    started=false;
+  }
+}
+//-------------------------------------
 //gets the next color for sequence
 function getNextColor(){
   level++;
@@ -46,19 +69,15 @@ function getNextColor(){
   switch (nextColor) {
     case 0:
       colorSequence.push("red");
-      $("#red").fadeIn(100).fadeOut(100).fadeIn(100);
       break;
     case 1:
       colorSequence.push("green");
-      $("#green").fadeIn(100).fadeOut(100).fadeIn(100);
       break;
     case 2:
       colorSequence.push("blue");
-      $("#blue").fadeIn(100).fadeOut(100).fadeIn(100);
       break;
     case 3:
       colorSequence.push("yellow");
-      $("#yellow").fadeIn(100).fadeOut(100).fadeIn(100);
       break;
   }
 };
@@ -81,76 +100,24 @@ function lightUp(button){
   switch (nextColor) {
     case 0:
       $("#red").fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound("0");
       break;
     case 1:
       $("#green").fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound("1")
       break;
     case 2:
       $("#blue").fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound("3")
       break;
     case 3:
       $("#yellow").fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound("4")
       break;
   }
 };
 
-// function startGame() {
-//   colorSequence = [];
-//   userInput = [];
-//   correct=true;
-//   while (correct === true) {
-//     getNextColor(nextSequence());
-//     console.log(colorSequence);
-//     for(let i=0; i<colorSequence.length; i++){
-//       let buttonColor = (colorSelection.indexOf(colorSequence[i]));
-//       lightButton(buttonColor);
-//       console.log(buttonColor);
-//       console.log("start");
-//       sleep(3000);
-//       console.log("end");
-//     };
-//     sleep(4000);
-//     console.log("pause over");
-//     if (userInput === colorSequence) {
-//       // alert("you got it right");
-//     } else {
-//       // alert("You got it wrong");
-//       correct=false;
-//       console.log("game ended");
-//       break;
-//     }
-//   }
-// };
-
-
-//
-//
-// //lights up buttons
-// function lightButton(buttonColor){
-//   switch (buttonColor) {
-//     case 0:
-//       console.log("red");
-//       document.getElementById("red").style.backgroundColor = "purple";
-//       sleep(2000);
-//       //document.getElementById("red").style.backgroundColor = "red";
-//       break;
-//     case 1:
-//       console.log("green");
-//       document.getElementById("green").style.backgroundColor = "purple";
-//       sleep(2000);
-//       //document.getElementById("green").style.backgroundColor = "green";
-//       break;
-//     case 2:
-//       console.log("blue");
-//       document.getElementById("blue").style.backgroundColor = "purple";
-//       sleep(5000);
-//       //document.getElementById("blue").style.backgroundColor = "blue";
-//       break;
-//     case 3:
-//       console.log("yellow");
-//       document.getElementById("yellow").style.backgroundColor = "purple";
-//       sleep(2000);
-//       //document.getElementById("yellow").style.backgroundColor = "yellow";
-//       break;
-//   }
-// };
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
